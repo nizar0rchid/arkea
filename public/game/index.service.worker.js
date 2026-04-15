afb55afb0fc290e7339b0da9c7c89bc80accbf0f
nizar-zembra <nizar@zembratech.com>
2026-04-15 14:24:41 +0100

    update game relase

--- public/game/index.service.worker.js
+++ public/game/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1774802509|485428188';
+const CACHE_VERSION = '1776183342|2742736249';
 /** @type {string} */
 const CACHE_PREFIX = 'ArkeA-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
