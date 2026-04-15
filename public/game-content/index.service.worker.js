81b20032502337bfa63b81bdd18aaefe1eb2946d
nizar-zembra <nizar@zembratech.com>
2026-04-15 15:53:29 +0100

    more agressive orientation lock

--- public/game-content/index.service.worker.js
+++ public/game-content/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1776264469|21021608';
+const CACHE_VERSION = '1776264764|20428640';
 /** @type {string} */
 const CACHE_PREFIX = 'ArkeA-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
