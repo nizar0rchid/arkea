0183ee24adcb245fa84ad11ab9c8a191e87f3a1b
nizar-zembra <nizar@zembratech.com>
2026-04-15 15:14:54 +0100

    update godot orientation

--- public/game-content/index.service.worker.js
+++ public/game-content/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1776262148|218069607';
+const CACHE_VERSION = '1776262371|441258590';
 /** @type {string} */
 const CACHE_PREFIX = 'ArkeA-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
