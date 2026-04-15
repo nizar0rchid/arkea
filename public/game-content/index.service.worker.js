8519a2d39178020f55ac06ee4529fe61acc8c2fa
nizar-zembra <nizar@zembratech.com>
2026-04-15 15:09:56 +0100

    test godot portrait export settings

--- public/game-content/index.service.worker.js
+++ public/game-content/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1776261141|90001412';
+const CACHE_VERSION = '1776262148|218069607';
 /** @type {string} */
 const CACHE_PREFIX = 'ArkeA-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
