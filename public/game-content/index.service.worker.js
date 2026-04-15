9c6a6f92053e338b03ade3387fd4b161623812a3
nizar-zembra <nizar@zembratech.com>
2026-04-15 16:06:33 +0100

    fix fullscreen reset

--- public/game-content/index.service.worker.js
+++ public/game-content/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1776265129|24084442';
+const CACHE_VERSION = '1776265532|11001771';
 /** @type {string} */
 const CACHE_PREFIX = 'ArkeA-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
