8dcdf5171babfa046f195a4234d7b60846ff2be8
YoussefChaabouni <youssefchaabouni98@gmail.com>
2026-04-25 18:51:26 +0100

    beta version with minor fixes and fps cap to 60fps

--- public/game-content/index.service.worker.js
+++ public/game-content/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1776991069|7514753126';
+const CACHE_VERSION = '1777124192|4010559255';
 /** @type {string} */
 const CACHE_PREFIX = 'ArkeA-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
