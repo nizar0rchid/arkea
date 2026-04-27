3c4ec5adaa4b3ab10d3d714637a3a2f41c2161a9
YoussefChaabouni <youssefchaabouni98@gmail.com>
2026-04-27 21:36:58 +0100

    beta version

--- public/game-content/index.service.worker.js
+++ public/game-content/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1777124192|4010559255';
+const CACHE_VERSION = '1777322112|38767993';
 /** @type {string} */
 const CACHE_PREFIX = 'ArkeA-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
