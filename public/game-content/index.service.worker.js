f661ccef655e2d97a68d3d54434b8fef4fe646d6
nizar-zembra <nizar@zembratech.com>
2026-04-29 00:12:40 +0100

    stable version

--- public/game-content/index.service.worker.js
+++ public/game-content/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1777322112|38767993';
+const CACHE_VERSION = '1777417809|123159517';
 /** @type {string} */
 const CACHE_PREFIX = 'ArkeA-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
