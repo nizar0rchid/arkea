77576773dc6fb32e564a2c7bf6f063733d44f0bb
nizar-zembra <nizar@zembratech.com>
2026-04-30 22:10:46 +0100

    add arekea icons

--- public/game-content/index.service.worker.js
+++ public/game-content/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1777579313|29751668';
+const CACHE_VERSION = '1777583302|1190298137';
 /** @type {string} */
 const CACHE_PREFIX = 'ArkeA-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
