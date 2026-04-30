285226211d0685b48455b90cc73f51cda50dbb45
nizar-zembra <nizar@zembratech.com>
2026-04-30 23:58:43 +0100

    stable version: -update progress bar color -update app icons -update mobile controls assets -update nextjs metada

--- app/standalone-game/layout.tsx
+++ app/standalone-game/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 
 export const metadata: Metadata = {
     manifest: "/game-content/index.manifest.json",
+    title: "ArkeA - Trial Of The Elements",
+    description: "ArkeA - Trial Of The Elements"
 };
 
 export default function StandaloneGameLayout({ children }: { children: React.ReactNode }) {
--- public/game-content/index.service.worker.js
+++ public/game-content/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1777584639|122548728';
+const CACHE_VERSION = '1777589723|15551682';
 /** @type {string} */
 const CACHE_PREFIX = 'ArkeA-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
