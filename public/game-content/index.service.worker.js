dbd99e4df09563a7cb3388b406cfbb5b2ebd4113
nizar-zembra <nizar@zembratech.com>
2026-04-30 22:32:54 +0100

    update arkea icons

--- public/game-content/index.service.worker.js
+++ public/game-content/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1777583302|1190298137';
+const CACHE_VERSION = '1777584639|122548728';
 /** @type {string} */
 const CACHE_PREFIX = 'ArkeA-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
