266e3ae740ebb1d14559c3c05aa75fd4e187ee6d
nizar-zembra <nizar@zembratech.com>
2026-05-11 12:06:34 +0100

    fix: interact button press radius

--- public/game-content/index.service.worker.js
+++ public/game-content/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1777715403|23066355';
+const CACHE_VERSION = '1778497459|194609734';
 /** @type {string} */
 const CACHE_PREFIX = 'ArkeA-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
