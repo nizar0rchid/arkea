024498f09225db59230cb43732e592114b27f379
nizar-zembra <nizar@zembratech.com>
2026-08-01 17:42:35 +0100

    feat: final zokom version

--- public/game-content/index.service.worker.js
+++ public/game-content/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1785597634|124468413';
+const CACHE_VERSION = '1785602491|1379795005';
 /** @type {string} */
 const CACHE_PREFIX = 'ArkeA-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
