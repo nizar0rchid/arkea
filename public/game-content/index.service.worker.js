4435acbd420b5a8ca8e5d6cb546bc19015fd8b11
nizar-zembra <nizar@zembratech.com>
2026-08-01 18:27:57 +0100

    feat: leaderboard polish

--- public/game-content/index.service.worker.js
+++ public/game-content/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1785602491|1379795005';
+const CACHE_VERSION = '1785605115|16854127';
 /** @type {string} */
 const CACHE_PREFIX = 'ArkeA-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
